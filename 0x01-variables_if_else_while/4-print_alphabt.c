61da1c529d9365f6faf65ea385b097f12b3aaba2
kayus143 <kayus143@gmail.com>
2023-03-19 15:40:38 -0700

    numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,11 +5,12 @@
  *
  * Return: always 0
  */
+
 int main (void)
 {
         char i;
-
-        for (i = 'a' ; i <= 'z' ; i++)
+       
+	for (i = 'a' ; i <= 'z' ; i++)
 	if (i != 'q' && i != 'e')
 	putchar(i);
 	putchar('\n');
